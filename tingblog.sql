23336b5f4fa3f64922eeed3f96650cf3ae87638d
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-04-21 11:11:12 +0530

    db

---
+++ tingblog.sql
@@ -0,0 +1,547 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 21, 2016 at 07:39 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tingblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accesslevel`
+--
+
+CREATE TABLE `accesslevel` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accesslevel`
+--
+
+INSERT INTO `accesslevel` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'Operator'),
+(3, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logintype`
+--
+
+CREATE TABLE `logintype` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logintype`
+--
+
+INSERT INTO `logintype` (`id`, `name`) VALUES
+(1, 'Facebook'),
+(2, 'Twitter'),
+(3, 'Email'),
+(4, 'Google');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `keyword` varchar(255) NOT NULL,
+  `url` text NOT NULL,
+  `linktype` int(11) NOT NULL,
+  `parent` int(11) NOT NULL,
+  `isactive` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `icon` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `name`, `description`, `keyword`, `url`, `linktype`, `parent`, `isactive`, `order`, `icon`) VALUES
+(1, 'Users', '', '', 'site/viewusers', 1, 0, 1, 1, 'icon-user'),
+(3, 'Blog', '', '', 'site/viewblog', 1, 0, 1, 2, 'icon-dashboard'),
+(4, 'Dashboard', '', '', 'site/index', 1, 0, 1, 0, 'icon-dashboard'),
+(5, 'Tags', '', '', 'site/viewtags', 1, 0, 1, 3, 'icon-dashboard'),
+(6, 'GIF', '', '', 'site/viewgif', 1, 0, 1, 4, 'icon-dashboard');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menuaccess`
+--
+
+CREATE TABLE `menuaccess` (
+  `menu` int(11) NOT NULL,
+  `access` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menuaccess`
+--
+
+INSERT INTO `menuaccess` (`menu`, `access`) VALUES
+(1, 1),
+(4, 1),
+(2, 1),
+(3, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(7, 3),
+(8, 1),
+(9, 1),
+(10, 1),
+(11, 1),
+(12, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pageaccesslevel`
+--
+
+CREATE TABLE `pageaccesslevel` (
+  `id` int(11) NOT NULL,
+  `page` int(11) NOT NULL,
+  `accesslevel` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pageaccesslevel`
+--
+
+INSERT INTO `pageaccesslevel` (`id`, `page`, `accesslevel`) VALUES
+(1, 4, 'user'),
+(2, 4, 'admin'),
+(3, 2, 'admin'),
+(4, 2, 'operator');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projectaccesslevel`
+--
+
+CREATE TABLE `projectaccesslevel` (
+  `id` int(11) NOT NULL,
+  `project` int(11) NOT NULL,
+  `accesslevel` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `projectaccesslevel`
+--
+
+INSERT INTO `projectaccesslevel` (`id`, `project`, `accesslevel`) VALUES
+(2, 1, 'admin'),
+(3, 2, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statuses`
+--
+
+CREATE TABLE `statuses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `statuses`
+--
+
+INSERT INTO `statuses` (`id`, `name`) VALUES
+(1, 'inactive'),
+(2, 'Active'),
+(3, 'Waiting'),
+(4, 'Active Waiting'),
+(5, 'Blocked');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tagsblog`
+--
+
+CREATE TABLE `tagsblog` (
+  `id` int(11) NOT NULL,
+  `blog` int(11) NOT NULL,
+  `tag` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tagsblog`
+--
+
+INSERT INTO `tagsblog` (`id`, `blog`, `tag`) VALUES
+(1, 2, 3),
+(2, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tingblog_blog`
+--
+
+CREATE TABLE `tingblog_blog` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `video` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tingblog_blog`
+--
+
+INSERT INTO `tingblog_blog` (`id`, `name`, `image`, `video`, `description`, `timestamp`) VALUES
+(2, 'asd', '16.png', 'asa', 'sadsfsd', '2016-04-20 12:17:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tingblog_gif`
+--
+
+CREATE TABLE `tingblog_gif` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tingblog_gif`
+--
+
+INSERT INTO `tingblog_gif` (`id`, `image`) VALUES
+(1, 'win2.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tingblog_tags`
+--
+
+CREATE TABLE `tingblog_tags` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tingblog_tags`
+--
+
+INSERT INTO `tingblog_tags` (`id`, `name`) VALUES
+(2, 'tag1'),
+(3, 'tag2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `title`
+--
+
+CREATE TABLE `title` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `logo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `accesslevel` int(11) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(11) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `socialid` varchar(255) NOT NULL,
+  `logintype` varchar(255) NOT NULL,
+  `json` text NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `billingaddress` text NOT NULL,
+  `billingcity` varchar(255) NOT NULL,
+  `billingstate` varchar(255) NOT NULL,
+  `billingcountry` varchar(255) NOT NULL,
+  `billingcontact` varchar(255) NOT NULL,
+  `billingpincode` varchar(255) NOT NULL,
+  `shippingaddress` text NOT NULL,
+  `shippingcity` varchar(255) NOT NULL,
+  `shippingcountry` varchar(255) NOT NULL,
+  `shippingstate` varchar(255) NOT NULL,
+  `shippingpincode` varchar(255) NOT NULL,
+  `shippingname` varchar(255) NOT NULL,
+  `shippingcontact` varchar(255) NOT NULL,
+  `currency` varchar(255) NOT NULL,
+  `credit` varchar(255) NOT NULL,
+  `companyname` varchar(255) NOT NULL,
+  `registrationno` varchar(255) NOT NULL,
+  `vatnumber` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `fax` varchar(255) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `facebook` varchar(255) NOT NULL,
+  `google` varchar(255) NOT NULL,
+  `twitter` varchar(255) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `address` text NOT NULL,
+  `dob` date NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `pincode` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `accesslevel`, `timestamp`, `status`, `image`, `username`, `socialid`, `logintype`, `json`, `firstname`, `lastname`, `phone`, `billingaddress`, `billingcity`, `billingstate`, `billingcountry`, `billingcontact`, `billingpincode`, `shippingaddress`, `shippingcity`, `shippingcountry`, `shippingstate`, `shippingpincode`, `shippingname`, `shippingcontact`, `currency`, `credit`, `companyname`, `registrationno`, `vatnumber`, `country`, `fax`, `gender`, `facebook`, `google`, `twitter`, `street`, `address`, `dob`, `city`, `state`, `pincode`) VALUES
+(1, 'wohlig', 'a63526467438df9566c508027d9cb06b', 'wohlig@wohlig.com', 1, '0000-00-00 00:00:00', 1, 'images_(2)1.jpg', '', '', 'Facebook', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '', '', '', '', '0000-00-00', '', '', ''),
+(6, 'Pooja Thakare', '', 'pooja.wohlig@gmail.com', 3, '2015-12-09 06:02:37', 1, 'https://lh5.googleusercontent.com/-5B1PwZZrwdI/AAAAAAAAAAI/AAAAAAAAABw/J3Hf871N8IE/photo.jpg', '', '103402210128529539675', 'Google', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '', '103402210128529539675', '', '', '', '0000-00-00', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlog`
+--
+
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL,
+  `onuser` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `onuser`, `status`, `description`, `timestamp`) VALUES
+(1, 1, 1, 'User Address Edited', '2014-05-12 06:50:21'),
+(2, 1, 1, 'User Details Edited', '2014-05-12 06:51:43'),
+(3, 1, 1, 'User Details Edited', '2014-05-12 06:51:53'),
+(4, 4, 1, 'User Created', '2014-05-12 06:52:44'),
+(5, 4, 1, 'User Address Edited', '2014-05-12 12:31:48'),
+(6, 23, 2, 'User Created', '2014-10-07 06:46:55'),
+(7, 24, 2, 'User Created', '2014-10-07 06:48:25'),
+(8, 25, 2, 'User Created', '2014-10-07 06:49:04'),
+(9, 26, 2, 'User Created', '2014-10-07 06:49:16'),
+(10, 27, 2, 'User Created', '2014-10-07 06:52:18'),
+(11, 28, 2, 'User Created', '2014-10-07 06:52:45'),
+(12, 29, 2, 'User Created', '2014-10-07 06:53:10'),
+(13, 30, 2, 'User Created', '2014-10-07 06:53:33'),
+(14, 31, 2, 'User Created', '2014-10-07 06:55:03'),
+(15, 32, 2, 'User Created', '2014-10-07 06:55:33'),
+(16, 33, 2, 'User Created', '2014-10-07 06:59:32'),
+(17, 34, 2, 'User Created', '2014-10-07 07:01:18'),
+(18, 35, 2, 'User Created', '2014-10-07 07:01:50'),
+(19, 34, 2, 'User Details Edited', '2014-10-07 07:04:34'),
+(20, 18, 2, 'User Details Edited', '2014-10-07 07:05:11'),
+(21, 18, 2, 'User Details Edited', '2014-10-07 07:05:45'),
+(22, 18, 2, 'User Details Edited', '2014-10-07 07:06:03'),
+(23, 7, 6, 'User Created', '2014-10-17 06:22:29'),
+(24, 7, 6, 'User Details Edited', '2014-10-17 06:32:22'),
+(25, 7, 6, 'User Details Edited', '2014-10-17 06:32:37'),
+(26, 8, 6, 'User Created', '2014-11-15 12:05:52'),
+(27, 9, 6, 'User Created', '2014-12-02 10:46:36'),
+(28, 9, 6, 'User Details Edited', '2014-12-02 10:47:34'),
+(29, 4, 6, 'User Details Edited', '2014-12-03 10:34:49'),
+(30, 4, 6, 'User Details Edited', '2014-12-03 10:36:34'),
+(31, 4, 6, 'User Details Edited', '2014-12-03 10:36:49'),
+(32, 8, 6, 'User Details Edited', '2014-12-03 10:47:16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accesslevel`
+--
+ALTER TABLE `accesslevel`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `logintype`
+--
+ALTER TABLE `logintype`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pageaccesslevel`
+--
+ALTER TABLE `pageaccesslevel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `projectaccesslevel`
+--
+ALTER TABLE `projectaccesslevel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `statuses`
+--
+ALTER TABLE `statuses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tagsblog`
+--
+ALTER TABLE `tagsblog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tingblog_blog`
+--
+ALTER TABLE `tingblog_blog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tingblog_gif`
+--
+ALTER TABLE `tingblog_gif`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tingblog_tags`
+--
+ALTER TABLE `tingblog_tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `title`
+--
+ALTER TABLE `title`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accesslevel`
+--
+ALTER TABLE `accesslevel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `logintype`
+--
+ALTER TABLE `logintype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `pageaccesslevel`
+--
+ALTER TABLE `pageaccesslevel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `projectaccesslevel`
+--
+ALTER TABLE `projectaccesslevel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `statuses`
+--
+ALTER TABLE `statuses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `tagsblog`
+--
+ALTER TABLE `tagsblog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tingblog_blog`
+--
+ALTER TABLE `tingblog_blog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tingblog_gif`
+--
+ALTER TABLE `tingblog_gif`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tingblog_tags`
+--
+ALTER TABLE `tingblog_tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `title`
+--
+ALTER TABLE `title`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
